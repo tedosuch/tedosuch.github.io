a75ef60a8194fd1eee7b1db13ab200f34f920043
Ted Osuch <tosuch18@gmail.com>
2025-08-29 12:24:02 +0100

    Update meta js
    Update JS

--- latin/flashcardgame.js
+++ latin/flashcardgame.js
@@ -1,13 +1,13 @@
 
 const flashcards = [
-  { question: "dedit", info: "{noun}",lesson: "LNM, 1-1", answer: "gave" },
-  { question: "dixit", info: "{noun}",lesson: "LNM, 1-1", answer: "said",},
-  { question: "fuit", info: "{noun}",lesson: "LNM, 1-1", answer: "had" },
-  { question: "h&aacute;buit", info: "{noun}",lesson: "LNM, 1-1", answer: "was" },
-  { question: "invenit", info: "{noun}",lesson: "LNM, 1-1", answer: "found" },
-  { question: "venit", info: "{noun}",lesson: "LNM, 1-1", answer: "came" },
-  { question: "vidit", info: "{noun}",lesson: "LNM, 1-1", answer: "saw" },
-  { question: "ad", info: "{noun}",lesson: "LNM, 1-1", answer: "to" },
+  { question: "dedit", info: "{verb}",lesson: "LNM, 1-1", answer: "gave" },
+  { question: "dixit", info: "{verb}",lesson: "LNM, 1-1", answer: "said",},
+  { question: "fuit", info: "{verb}",lesson: "LNM, 1-1", answer: "had" },
+  { question: "h&aacute;buit", info: "{verb}",lesson: "LNM, 1-1", answer: "was" },
+  { question: "invenit", info: "{verb}",lesson: "LNM, 1-1", answer: "found" },
+  { question: "venit", info: "{verb}",lesson: "LNM, 1-1", answer: "came" },
+  { question: "vidit", info: "{verb}",lesson: "LNM, 1-1", answer: "saw" },
+  { question: "ad", info: "{preposition}",lesson: "LNM, 1-1", answer: "to" },
 ];
 
 const container = document.getElementById("flashcard-container");
@@ -16,10 +16,17 @@ flashcards.forEach((cardData) => {
   const card = document.createElement("div");
   card.classList.add("card");
 
-  card.innerHTML = `
+  // card.innerHTML = `
+  //   <div class="card-inner">
+  //     <div class="card-front">${cardData.question}<p class="meta">${cardData.info} - ${cardData.lesson}</p> </div>
+  //     <div class="card-back">${cardData.answer}</div>
+  //   </div>
+  // `;
+
+    card.innerHTML = `
     <div class="card-inner">
-      <div class="card-front">${cardData.question}<p class="test">${cardData.info} - ${cardData.lesson}</p> </div>
-      <div class="card-back">${cardData.answer}</div>
+      <div class="card-front">${cardData.question}<p class="metalatin">${cardData.info} - ${cardData.lesson}</p> </div>
+      <div class="card-back">${cardData.answer}<p class="metaenglish">${cardData.info} - ${cardData.lesson}</p> </div>
     </div>
   `;
 
@@ -29,3 +36,9 @@ flashcards.forEach((cardData) => {
 
   container.appendChild(card);
 });
+
+/* ADD MENU HERE */
+
+
+
+ 
